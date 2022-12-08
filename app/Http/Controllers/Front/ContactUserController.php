ece8b6e5371f418c5de42c6702ab90b104658371
Nguyễn Quang Huy <643007@sv.vnua.edu.vn>
2022-12-08 16:32:14 +0700

    xem, cập nhật thông tin KH/shop

--- app/Http/Controllers/Front/ContactUserController.php
+++ app/Http/Controllers/Front/ContactUserController.php
@@ -13,91 +13,91 @@ use Illuminate\Support\Facades\Auth;
 
 class ContactUserController extends Controller
 {
-    private $productCategoryService;
-    private $userService;
+  private $productCategoryService;
+  private $userService;
 
 
-    public function __construct(ProductCategoryServiceInterface $productCategoryService, UserServiceInterface $userService)
-    {
-        $this->productCategoryService = $productCategoryService;
-        $this->userService = $userService;
-    }
+  public function __construct(ProductCategoryServiceInterface $productCategoryService, UserServiceInterface $userService)
+  {
+    $this->productCategoryService = $productCategoryService;
+    $this->userService = $userService;
+  }
 
-    public function index()
-    {
+  public function index()
+  {
+    $categories = $this->productCategoryService->all();
+    return view('front.account.my-contact.index', compact('categories'));
+  }
 
-        $categories = $this->productCategoryService->all();
-        return view('front.account.my-contact.index', compact('categories'));
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $categories = $this->productCategoryService->all();
+    return view('front.account.my-contact.index', compact('categories', 'id'));
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $categories = $this->productCategoryService->all();
+    return view('front.account.my-contact.edit', compact('categories', 'id'));
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit()
-    {
-        $categories = $this->productCategoryService->all();
-        return view('front.account.my-contact.edit', compact('categories'));
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $data = $request->all();
+    //cập nhật dữ liệu
+    $this->userService->update($data, $id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request)
-    {
-        $data = $request->all();
-        //cập nhật dữ liệu
-        $this->userService->update($data, Auth::user()->id);
+    return redirect('account/my-contact/contactuser');
+  }
 
-        return redirect('front.account.my-contact');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
